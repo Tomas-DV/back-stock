8da4cb0d381eedb29d0ea9fec72193729a06b8ab
HKS <tomas.corsico@hks.ar>
2023-10-10 20:24:51 -0300

    First upload

---
+++ src/test/java/com/stock/backStock/BackStockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stock.backStock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackStockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
